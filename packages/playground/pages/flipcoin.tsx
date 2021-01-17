e6efbd62c62af9a99c78fe1eea766592986dc3a7
han0110 <tinghan0110@gmail.com>
2021-01-17 21:47:17 +0800

    feat: add favicon for flipcoin

--- packages/playground/pages/flipcoin.tsx
+++ packages/playground/pages/flipcoin.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Head from 'next/head'
 import { useSubscription } from '@apollo/client'
 import { Grid, GridItem } from '@chakra-ui/react'
 // Component
@@ -15,29 +16,38 @@ const FlipCoinPage = () => {
   const subscription = useSubscription<SubscribePlayerData>(SUBSCRIBE_PLAYER)
   // Render
   return (
-    <Layout
-      mainProps={{
-        maxW: ['90vw', '80rem'],
-        display: 'flex',
-        alignItems: 'stretch',
-      }}
-    >
-      <Grid
-        my="5"
-        flex={1}
-        templateRows={['0 80vh 1fr', '0 80vh 1fr', 'auto']}
-        templateColumns={['auto', 'auto', '1fr minmax(200px, max-content) 1fr']}
-        gap={[4, 4, 10]}
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon/flipcoin.ico" />
+      </Head>
+      <Layout
+        mainProps={{
+          maxW: ['90vw', '80rem'],
+          display: 'flex',
+          alignItems: 'stretch',
+        }}
       >
-        <GridItem />
-        <GridItem display="flex">
-          <FlipCoin subscription={subscription} />
-        </GridItem>
-        <GridItem display="flex" overflowX="hidden">
-          <LeaderBoard subscription={subscription} />
-        </GridItem>
-      </Grid>
-    </Layout>
+        <Grid
+          my="5"
+          flex={1}
+          templateRows={['0 80vh 1fr', '0 80vh 1fr', 'auto']}
+          templateColumns={[
+            'auto',
+            'auto',
+            '1fr minmax(200px, max-content) 1fr',
+          ]}
+          gap={[4, 4, 10]}
+        >
+          <GridItem />
+          <GridItem display="flex">
+            <FlipCoin subscription={subscription} />
+          </GridItem>
+          <GridItem display="flex" overflowX="hidden">
+            <LeaderBoard subscription={subscription} />
+          </GridItem>
+        </Grid>
+      </Layout>
+    </>
   )
 }
 
